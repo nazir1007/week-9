30d3ff36a5671a050dbaf48c9215293e80ebe6bb
Mohd Nazir <mohdnazir1007@gmail.com>
2023-08-12 16:21:15 +0530

    changes

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,2 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+# ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "nazir_todo_app.pem" ubuntu@ec2-13-127-14-218.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
